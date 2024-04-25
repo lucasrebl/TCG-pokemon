2e648cea762c51639e5cb8399cb1197f9d79f905
RLUCAS <lucas.reboulet@ynov.com>
2024-04-25 16:29:33 +0200

    function for read packs name in page profil

--- src/controller/profilController.php
+++ src/controller/profilController.php
@@ -4,11 +4,13 @@ use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
 require 'vendor/autoload.php';
+require_once 'database/connect.php';
 
 class profilController
 {
     protected $twig;
     private $loader;
+    private $dsn;
 
     public function __construct()
     {
@@ -23,13 +25,21 @@ class profilController
         $user = [
             'idUser' => $_SESSION['idUser']
         ];
-
         if ($user['idUser'] < 1) {
             echo "veuiller vous connecter ou créer un compte pour avoir accés a votre profil";
         }
 
-        echo $this->twig->render('profil/profil.html.twig');
         $this->logOut();
+        $packsName = $this->getPackByIdUser();
+        echo $this->twig->render('profil/profil.html.twig', [
+            'readPacksName' => $packsName
+        ]);
+    }
+
+    public function connectDb()
+    {
+        $this->dsn = new PDO("mysql:host=mysql;dbname=my_database", "my_user", "my_password");
+        $this->dsn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
     public function logOut()
@@ -39,5 +49,23 @@ class profilController
             session_unset();
             echo '<script>window.location.replace("/login");</script>';
         }
+    }
+
+    public function getPackByIdUser()
+    {
+        $user = [
+            'idUser' => $_SESSION['idUser']
+        ];
+        $idUser = $user['idUser'];
+
+        $this->connectDb();
+
+        $query = "SELECT name FROM packs WHERE idUser = :idUser";
+        $stmt = $this->dsn->prepare($query);
+        $stmt->bindParam(':idUser', $idUser, PDO::PARAM_INT);
+        $stmt->execute();
+        $packsName = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        return $packsName;
     }
 }
